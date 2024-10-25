84481a4845654b8d7f0c891993e68bbdfb67c100
CA01971028 <107534406+CA01971028@users.noreply.github.com>
2024-10-25 11:02:30 +0900

    ヘッダーを作成
    スレッド作成におけるヘッダーをtailwindcssを利用し作成。

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div>
+            <div className="bg-lime-400  h-20 w-full flex flex-row justify-between">
+                <div className="text-4xl text-white px-4 py-4">掲示板</div>
+                <div className="text-2xl text-white px-4 py-4">スレッドをたてる</div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Header;
---
+++ my-react-app/src/components/Thread.jsx
@@ -0,0 +1,9 @@
+const Thread = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Thread;
